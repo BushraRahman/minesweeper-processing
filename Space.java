b603aa5ac8a864f273b0c3b841c7008559903396
Bushra Rahman <agario4ev@gmail.com>
2023-05-22 14:37:39 -0400

    Added variables and methods to Space class

---
+++ Space.java
@@ -0,0 +1,14 @@
+public class Space{
+	String type;
+	boolean flag;
+	boolean uncovered;
+	int adjact;
+	void Space(){}
+	public String getType(){}
+	public void changeType(){}
+	public void changeAdjacent(){}
+	public int getAdjacent(){}
+	public boolean getUncovered(){}
+	public void changeUncovered(){}
+	public void drawSquare(int x, int y){}
+	}
